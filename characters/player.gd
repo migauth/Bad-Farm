ad562abd9354354b67fc338335fe8c269be8fb93
Michael Gauthier <mikegauthier222@hotmail.com>
2024-08-04 00:36:09 -0400

    add idle and movement animation

--- characters/player.gd
+++ characters/player.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 70
+@export var starting_direction : Vector2 = Vector2(0,1)
+
+@onready var animation_tree = $AnimationTree
+
+func _ready():
+	update_animation_parameters(starting_direction)
 
 func _physics_process(delta):
 	#Get input velocity
@@ -10,8 +16,15 @@ func _physics_process(delta):
 		
 	)
 	
+	update_animation_parameters(input_direction)
+	
 	#Update velocity
 	velocity = input_direction * move_speed
 	
 	#Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()
+
+func update_animation_parameters(move_input : Vector2):
+	if(move_input != Vector2.ZERO):
+			animation_tree.set("parameters/Walk/blend_position", move_input)
+			animation_tree.set("parameters/Idle/blend_position", move_input)
