fb0da86efed01e1544fa05a875d27c0140fa1ac3
Michael Gauthier <mikegauthier222@hotmail.com>
2024-08-05 13:18:05 -0400

    add cow animation and script

--- characters/player.gd
+++ characters/player.gd
@@ -14,8 +14,7 @@ func _physics_process(delta):
 	var input_direction = Vector2(
 		Input.get_action_strength("right") - Input.get_action_strength("left"),
 		Input.get_action_strength("down") - Input.get_action_strength("up")
-		
-	)
+	).normalized()
 	
 	update_animation_parameters(input_direction)
 	
