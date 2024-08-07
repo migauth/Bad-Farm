40e51420ec840802fe209afbdf3d9e644200e6ba
Michael Gauthier <mikegauthier222@hotmail.com>
2024-08-07 18:07:21 -0400

    add cow movement(in progress)

--- characters/cow.gd
+++ characters/cow.gd
@@ -2,16 +2,18 @@ extends CharacterBody2D
 
 enum COW_STATE { IDLE, WALK }
 
-@export var move_speed : float = 30
+@export var move_speed : float = 20
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
+@onready var sprite = $Sprite2D
 
 var move_direction : Vector2 = Vector2.ZERO
 var current_state : COW_STATE = COW_STATE.IDLE
 
 func _ready():
 	select_new_direction()
+	pick_new_state()
 	
 func _physics_process(delta):	
 	velocity = move_direction * move_speed
@@ -23,9 +25,16 @@ func select_new_direction():
 		randi_range(-1,1),
 		randi_range(-1,1)
 	)
+	
+	if (move_direction.x < 0):
+		sprite.flip_h = true
+	elif (move_direction.x > 0):
+		sprite.flip_h = false
 
 func pick_new_state():
 	if(current_state == COW_STATE.IDLE):
-		state_machine.travel("Walk")
+		state_machine.travel("walk_right")
+		current_state = COW_STATE.WALK
 	elif(current_state == COW_STATE.WALK):
-		state_machine.travel("Idle")
+		state_machine.travel("idle_right")
+		current_state = COW_STATE.IDLE
