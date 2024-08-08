cb48a4de23ff2677b5ca13c36813ad33e323b260
Michael Gauthier <mikegauthier222@hotmail.com>
2024-08-07 21:24:38 -0400

    new main player sprite

--- characters/cow.gd
+++ characters/cow.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 enum COW_STATE { IDLE, WALK }
 
 @export var move_speed : float = 20
+@export var idle_time : float = 5
+@export var walk_time : float = 2
 
 @onready var animation_tree = $AnimationTree
 @onready var state_machine = animation_tree.get("parameters/playback")
 @onready var sprite = $Sprite2D
+@onready var timer = $Timer
 
 var move_direction : Vector2 = Vector2.ZERO
 var current_state : COW_STATE = COW_STATE.IDLE
@@ -35,6 +38,9 @@ func pick_new_state():
 	if(current_state == COW_STATE.IDLE):
 		state_machine.travel("walk_right")
 		current_state = COW_STATE.WALK
+		select_new_direction()
+		timer.start(walk_time)
 	elif(current_state == COW_STATE.WALK):
 		state_machine.travel("idle_right")
 		current_state = COW_STATE.IDLE
+		timer.start(idle_time)
